5867ac9fa30b07d4b0e117b5b181a74cf221f291
skeres <stephane.keresztes@gmail.com>
2023-03-12 01:20:00 +0100

    ajout commentaires

---
+++ src/main/java/com/sks/demo/Util.java
@@ -0,0 +1,14 @@
+package com.sks.demo;
+
+public class Util {
+
+    public static final String QUARANTE_DEUX="42";
+
+    /**
+     * Classe minimal
+     * @return
+     */
+    public String return42() {
+        return QUARANTE_DEUX;
+    }
+}
